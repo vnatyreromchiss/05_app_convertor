631d13b55c8f94e8aee26039f5ade8bd55355f6b
vnatyreromchiss <rdg2039@gmail.com>
2022-12-04 10:14:18 -0600

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Block } from './Block';
 
 function App() {
+  const [fromCurrency, setFromCurrency] = React.useState('UAH');
+  const [toCurrency, setToCurrency] = React.useState('USD');
+  const [fromPrice, setFromPrice] = React.useState(0);
+  const [toPrice, setToPrice] = React.useState(0);
+
+  const [rates,setRates] = React.useState({});
+
+  React.useEffect(() => {
+     fetch('./rates.json')
+     .then((res) => res.json())
+     .then((json) => {
+       setRates(json.rates);
+       console.log(json.rates);
+     })
+     .catch((err) => {
+      //  console.warn(err);
+      //  alert('Не удалось получить информацию')
+     });
+   }, []);
+
+  
+
+  const onChangeFromPrice = (value) => {
+    const price = value / rates[fromCurrency];
+    const result = price * rates[toCurrency];
+    setToPrice(result);
+    setFromPrice(value);
+  }
+  const onChangeToPrice = (value) => {
+    const result = (rates[fromCurrency] / rates[toCurrency]) * value;
+    
+    setFromPrice(result);
+    setToPrice(value);
+  }
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Block value={fromPrice} currency={fromCurrency} onChangeCurrency={setFromCurrency} onChangeValue={onChangeFromPrice} />
+      <Block value={toPrice} currency={toCurrency} onChangeCurrency={setToCurrency}  onChangeValue={onChangeToPrice} />
     </div>
   );
 }
 
 export default App;
+
+
